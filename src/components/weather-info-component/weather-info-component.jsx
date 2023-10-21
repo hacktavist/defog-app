3bfaa9453ae0e0c2424d9b21178dab92ab59868f
NP <hacktavist616@gmail.com>
2023-10-21 15:45:08 -0400

    more sophisticated defogging function

--- src/components/weather-info-component/weather-info-component.jsx
+++ src/components/weather-info-component/weather-info-component.jsx
@@ -5,7 +5,7 @@ function WeatherInfo({ zipCode }) {
   const [weatherData, setWeatherData] = useState(null);
 
   useEffect(() => {
-    const API_KEY = "YOUR_TOMORROW_IO_API_KEY";
+    const API_KEY = "YOUR_API_KEY_HERE";
 
     // Check if zipCode is provided and not empty before making the API call
     if (zipCode && zipCode.length === 5) {
@@ -15,6 +15,7 @@ function WeatherInfo({ zipCode }) {
         )
         .then((response) => {
           setWeatherData(response.data);
+          console.log(response.data);
         })
         .catch((error) => {
           console.error("Error fetching weather data:", error);
@@ -31,17 +32,66 @@ function WeatherInfo({ zipCode }) {
     const temperature = weatherData.temperature;
     const humidity = weatherData.humidity;
     const precipitation = weatherData.precipitation;
-    const windSpeed = weatherData.windSpeed;
+    const season = getSeason();
 
     if (temperature < 32) {
-      return "Use front and rear window defrosters to clear ice.";
-    } else if (temperature > 32 && humidity > 80 && precipitation > 0) {
-      return "Use air conditioning with defrost to remove condensation.";
-    } else if (windSpeed > 20) {
-      return "Expect rapid fog dissipation due to high wind speeds.";
-    } else {
-      return "No specific defogging method required at the moment.";
+      return handleLowTemperature(temperature, humidity, precipitation);
     }
+
+    if (season === "Winter") {
+      return handleWinterConditions(precipitation, humidity);
+    }
+
+    if (season === "Summer") {
+      return handleSummerConditions(humidity, weatherData.visibility);
+    }
+
+    return "No specific defogging method required at the moment.";
+  }
+
+  function handleLowTemperature(temperature, humidity, precipitation) {
+    if (precipitation > 0) {
+      return humidity > 80
+        ? "Use front and rear window defrosters to clear ice and air conditioning with defrost to handle condensation from precipitation."
+        : "Use front and rear window defrosters to clear ice caused by precipitation.";
+    }
+    return "Use front and rear window defrosters to clear ice.";
+  }
+
+  function handleWinterConditions(precipitation, humidity) {
+    if (precipitation > 0) {
+      return humidity > 80
+        ? "Use air conditioning with defrost to remove condensation caused by snow or sleet."
+        : "Use air conditioning with defrost to remove condensation caused by snow or sleet.";
+    }
+
+    return "No specific defogging method required at the moment.";
+  }
+
+  function handleSummerConditions(humidity, visibility) {
+    if (humidity > 80) {
+      return "Use air conditioning with defrost to remove condensation caused by high humidity.";
+    }
+
+    if (visibility === "Low") {
+      return "Low visibility suggests potential fogging; use defogging methods if needed.";
+    }
+
+    return "No specific defogging method required at the moment.";
+  }
+
+  // Helper function to get the season based on the current month
+  function getSeason() {
+    const currentMonth = new Date().getMonth() + 1; // Month is 0-indexed
+    if (currentMonth >= 1 && currentMonth <= 3) {
+      return "Winter";
+    } else if (currentMonth >= 4 && currentMonth <= 6) {
+      return "Spring";
+    } else if (currentMonth >= 7 && currentMonth <= 9) {
+      return "Summer";
+    }
+
+    return "Fall";
   }
 
   const suggestedMethod = determineDefoggingMethod(weatherData);
